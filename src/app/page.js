4d6afddf9c65001b2cc68c0cef744ac2632901b2
meenal <meenalnimje@gmail.com>
2023-10-20 15:55:51 +0530

    css changes in footer

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,6 @@ export default function Home() {
     setSearchData(filterData)
   }
   return (
-
     <div className="home">
       <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-start max-h-full bg-[#ffffff]">
@@ -85,23 +84,5 @@ export default function Home() {
       </main>
       <Footer />
     </div>
-    <main className="flex min-h-screen flex-col items-center justify-start">
-      <h1 className="text-9xl font-bold z-20 mt-60">BacPac</h1>
-      <div className="search-box mt-4 border-1 border-black w-4/12 h-12 rounded-2xl">
-        <input
-          type="text"
-          onChange={handleSearch}
-          placeholder="Search Colleges..."
-          className="w-full h-full indent-2.5 rounded-2xl"
-        />
-        {open && (
-          <div className="searchBox border-2 overflow-auto border-gray-300 w-full h-80 mt-4 rounded-lg p-3 bg-white">
-            {searchData.map((item, index) => (
-              <CollegeResult info={item} serialNo={index} key={index} />
-            ))}
-          </div>
-        )}
-      </div>
-    </main>
   )
 }
