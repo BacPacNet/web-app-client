667c0005969565fd1bb920d07d2ccf5fcb1f2071
Ayush Tiwari <ayushtiwari110@yahoo.com>
2023-12-19 07:52:00 +0530

    BI-42: Done requested changes

--- src/utils/searchAlgorithm.js
+++ src/utils/searchAlgorithm.js
@@ -1,12 +1,5 @@
 import { matchSorter } from 'match-sorter'
-import universityData from '../../data/university_data.json'
-
-const colleges = universityData.map((college) => {
-  return {
-    name: college.name,
-    address: college.address,
-  }
-})
+import colleges from '../../data/university_data.json'
 
 const searchAlgorithm = (input) => {
   return matchSorter(colleges, input, { keys: ['name', 'address'] })
