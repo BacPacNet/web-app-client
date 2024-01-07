26bf75ca4cc19892a061343f24bed4aa03feaad4
meenal <meenalnimje@gmail.com>
2024-01-07 19:15:12 +0530

    fixing error in testing

--- src/__tests__/query.test.js
+++ src/__tests__/query.test.js
@@ -14,39 +14,32 @@ const query = gql`
     }
   }
 `
-async function fetchData() {
+test('the data is of college', async () => {
   const result = await client.query({ query })
-  return result
-}
-describe('test to check the data of collegeList', () => {
-  let fetchedData
-  beforeAll(async () => {
-    fetchedData = await fetchData()
-  })
-  it('the data is of college', () => {
-    expect(fetchedData?.data).toHaveProperty('universityList')
-    expect(fetchedData?.data?.universityList).toBeInstanceOf(Array)
-    expect(fetchedData?.data?.universityList?.length).toBeGreaterThan(0)
-  })
-  it('the data has specific properties', () => {
-    // Reuse fetchedData in the second test
-    fetchedData?.data?.universityList?.forEach((college) => {
-      // check for id property
-      expect(college).toHaveProperty('id')
-      expect(college?.id).not.toBeNull()
-      expect(typeof college?.id).toBe('string')
-      // check for name property
-      expect(college).toHaveProperty('name')
-      expect(college?.name).not.toBeNull()
-      expect(typeof college?.name).toBe('string')
-      // check for score property
-      expect(college).toHaveProperty('score')
-      expect(college?.score).not.toBeNull()
-      expect(typeof college?.score).toBe('string')
-      // check for country property
-      expect(college).toHaveProperty('country')
-      expect(college?.country).not.toBeNull()
-      expect(typeof college?.country).toBe('string')
-    })
-  }, 10000)
+  expect(result.data).not.toBeNull()
+  expect(result?.data).toHaveProperty('universityList')
+  expect(result?.data?.universityList).toBeInstanceOf(Array)
+  expect(result?.data?.universityList?.length).toBeGreaterThan(0)
 })
+// test to check the format of data recived by the query
+test('the data has specific properties', async () => {
+  const result = await client.query({ query })
+  result?.data?.universityList?.forEach((college) => {
+    // check for id property
+    expect(college).toHaveProperty('id')
+    expect(college?.id).not.toBeNull()
+    expect(typeof college?.id).toBe('string')
+    // check for name property
+    expect(college).toHaveProperty('name')
+    expect(college?.name).not.toBeNull()
+    expect(typeof college?.name).toBe('string')
+    // check for score property
+    expect(college).toHaveProperty('score')
+    expect(college?.score).not.toBeNull()
+    expect(typeof college?.score).toBe('string')
+    // check for country property
+    expect(college).toHaveProperty('country')
+    expect(college?.country).not.toBeNull()
+    expect(typeof college?.country).toBe('string')
+  })
+}, 10000)
