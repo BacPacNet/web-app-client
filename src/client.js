7aca49e92869cacc5679a1dd1d5f5d6fd11bc634
meenal <meenalnimje@gmail.com>
2024-01-07 20:45:27 +0530

    prev code to fix the error

--- src/client.js
+++ src/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import fetch from 'cross-fetch'
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: `http://localhost:3000/api/graphql`, fetch }),
+  link: new HttpLink({ uri: 'http://localhost:3000/api/graphql', fetch }),
   cache: new InMemoryCache(),
 })
 
