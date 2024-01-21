e852ff991264ee7e298db55f105ae6bd7bbfb0b8
meenal <meenalnimje@gmail.com>
2024-01-21 21:51:37 +0530

    vercal bot link added for test

--- src/client.js
+++ src/client.js
@@ -2,7 +2,10 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
 import fetch from 'cross-fetch'
 
-const uri = process.env.NODE_ENV == 'development' ? 'http://localhost:3000/api/graphql' : 'https://web-app-client-flame.vercel.app/api/graphql'
+const uri =
+  process.env.NODE_ENV == 'development'
+    ? 'http://localhost:3000/api/graphql'
+    : 'https://web-app-client-jg2xfkwxj-bacpacs-projects.vercel.app/api/graphql'
 const client = new ApolloClient({
   link: new HttpLink({ uri, fetch }),
   cache: new InMemoryCache(),
