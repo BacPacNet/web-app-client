8e9a9bf100b5ebbfc88a2c9cde915d306c2aefa0
meenal <meenalnimje@gmail.com>
2024-01-24 22:51:20 +0530

    after 2nd code review

--- src/__tests__/query.test.js
+++ src/__tests__/query.test.js
@@ -24,39 +24,31 @@ async function fetchData() {
 }
 // test to check the data is not empty
 test('the data is of college', async () => {
-  try {
-    const result = await fetchData()
-    expect(result?.data).not.toBeNull()
-    expect(result?.data).toHaveProperty('universityList')
-    expect(result?.data?.universityList).toBeInstanceOf(Array)
-    expect(result?.data?.universityList?.length).toBeGreaterThan(0)
-  } catch (e) {
-    expect(e.message).toMatch('error')
-  }
+  const result = await fetchData()
+  expect(result?.data).not.toBeNull()
+  expect(result?.data).toHaveProperty('universityList')
+  expect(result?.data?.universityList).toBeInstanceOf(Array)
+  expect(result?.data?.universityList?.length).toBeGreaterThan(0)
 })
 // test to check the format of data recived by the query
 test('the data has specific properties', async () => {
-  try {
-    const result = await fetchData()
-    result?.data?.universityList?.forEach((college) => {
-      // check for id property
-      expect(college).toHaveProperty('id')
-      expect(college?.id).not.toBeNull()
-      expect(typeof college?.id).toBe('string')
-      // check for name property
-      expect(college).toHaveProperty('name')
-      expect(college?.name).not.toBeNull()
-      expect(typeof college?.name).toBe('string')
-      // check for score property
-      expect(college).toHaveProperty('score')
-      expect(college?.score).not.toBeNull()
-      expect(typeof college?.score).toBe('string')
-      // check for country property
-      expect(college).toHaveProperty('country')
-      expect(college?.country).not.toBeNull()
-      expect(typeof college?.country).toBe('string')
-    })
-  } catch (e) {
-    expect(e.message).toMatch('error')
-  }
+  const result = await fetchData()
+  result?.data?.universityList?.forEach((college) => {
+    // check for id property
+    expect(college).toHaveProperty('id')
+    expect(college?.id).not.toBeNull()
+    expect(typeof college?.id).toBe('string')
+    // check for name property
+    expect(college).toHaveProperty('name')
+    expect(college?.name).not.toBeNull()
+    expect(typeof college?.name).toBe('string')
+    // check for score property
+    expect(college).toHaveProperty('score')
+    expect(college?.score).not.toBeNull()
+    expect(typeof college?.score).toBe('string')
+    // check for country property
+    expect(college).toHaveProperty('country')
+    expect(college?.country).not.toBeNull()
+    expect(typeof college?.country).toBe('string')
+  })
 })
--- src/client.js
+++ src/client.js
@@ -2,10 +2,8 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
 import fetch from 'cross-fetch'
 
-const uri =
-  process.env.NODE_ENV == 'development'
-    ? 'http://localhost:3000/api/graphql'
-    : 'https://web-app-client-jg2xfkwxj-bacpacs-projects.vercel.app/api/graphql'
+const uri = process.env.LOCAL_STATE === 'development' ? 'http://localhost:3000/api/graphql' : 'https://web-app-client-flame.vercel.app/api/graphql'
+console.log('graphql_uri', uri)
 const client = new ApolloClient({
   link: new HttpLink({ uri, fetch }),
   cache: new InMemoryCache(),
