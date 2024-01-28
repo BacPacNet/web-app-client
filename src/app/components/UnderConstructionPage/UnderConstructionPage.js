1a955e6da3cc708c104d1dc45bfb67ccc94ba276
SATYAM KUMAR <106728045+satyam-x10@users.noreply.github.com>
2024-01-29 00:01:00 +0530

    BI-54 : test template (#26)

---
+++ src/app/components/UnderConstructionPage/UnderConstructionPage.js
@@ -0,0 +1,31 @@
+import './style.css'
+
+function UnderConstructionPage() {
+    return (
+        <div className="under-construction-page">
+            <div class="uc__wrapper">
+                <div class="uc__details">
+                    <h1 class="title">Coming Soon!</h1>
+                    <h3 class="intro">
+                        We are working hard to give you a better experience.
+                    </h3>
+                    <p class="uc__description">
+                        We are working hard on it & the site may go live very soon. We promise, it will be worth the wait!
+                    </p>
+                    <div class="uc__subscribe">
+                        <h3>Get Notified When We Go Live</h3>
+
+                        <div class="uc__form">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="uc__art">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UnderConstructionPage
