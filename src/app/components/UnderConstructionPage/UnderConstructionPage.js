23213eca020ccd6d9d71c9015c1ea5aa5507a919
SATYAM KUMAR <satyamx10@gmail.com>
2024-01-30 23:22:02 +0530

    BI-54: prettier fix

--- src/app/components/UnderConstructionPage/UnderConstructionPage.js
+++ src/app/components/UnderConstructionPage/UnderConstructionPage.js
@@ -1,63 +1,55 @@
-import './css/main.css';
-import './css/util.css';
+import './css/main.css'
+import './css/util.css'
 import './css/bootstrap.css'
 
-import React, { useEffect, useState, useMemo } from 'react';
-import { calculateTimeRemaining, startCountdown } from './js/script';
-
+import React, { useEffect, useState, useMemo } from 'react'
+import { calculateTimeRemaining, startCountdown } from './js/script'
 
 function UnderConstructionPage() {
-    const eventDate = useMemo(() => new Date("2024-06-01T00:00:00"), []);
+  const eventDate = useMemo(() => new Date('2024-06-01T00:00:00'), [])
 
-    const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining(eventDate));
+  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining(eventDate))
 
-    useEffect(() => {
-        // Start the countdown when the component mounts
-        const intervalId = startCountdown(eventDate, setTimeRemaining);
+  useEffect(() => {
+    // Start the countdown when the component mounts
+    const intervalId = startCountdown(eventDate, setTimeRemaining)
 
-        // Clean up the interval when the component unmounts
-        return () => clearInterval(intervalId);
-    }, [eventDate]);
-    return (
-        <div className="UnderConstructionPage">
-            <div className="bg-g1 size1 flex-w flex-col-c-sb p-l-15 p-r-15 p-t-55 p-b-35 respon1">
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId)
+  }, [eventDate])
+  return (
+    <div className="UnderConstructionPage">
+      <div className="bg-g1 size1 flex-w flex-col-c-sb p-l-15 p-r-15 p-t-55 p-b-35 respon1">
+        <div className="flex-col-c p-t-50 p-b-50">
+          <h3 className="l1-txt1 txt-center p-b-10">Coming Soon</h3>
 
-                <div className="flex-col-c p-t-50 p-b-50">
-                    <h3 className="l1-txt1 txt-center p-b-10">
-                        Coming Soon
-                    </h3>
+          <p className="txt-center l1-txt2 p-b-60">Our website is under construction</p>
 
-                    <p className="txt-center l1-txt2 p-b-60">
-                        Our website is under construction
-                    </p>
-
-                    <div className="flex-w flex-c cd100 p-b-82">
-                        <div className="flex-col-c-m size2 how-countdown">
-                            <div className="l1-txt3 p-b-9 days">{timeRemaining.days}</div>
-                            <div className="s1-txt1">Days</div>
-                        </div>
-
-                        <div className="flex-col-c-m size2 how-countdown">
-                            <div className="l1-txt3 p-b-9 hours">{timeRemaining.hours}</div>
-                            <div className="s1-txt1">Hours</div>
-                        </div>
-
-                        <div className="flex-col-c-m size2 how-countdown">
-                            <div className="l1-txt3 p-b-9 minutes">{timeRemaining.minutes}</div>
-                            <div className="s1-txt1">Minutes</div>
-                        </div>
-
-                        <div className="flex-col-c-m size2 how-countdown">
-                            <div className="l1-txt3 p-b-9 minutes">{timeRemaining.seconds}</div>
-                            <div className="s1-txt1">Seconds</div>
-                        </div>
-                    </div>
-                </div>
-
-
+          <div className="flex-w flex-c cd100 p-b-82">
+            <div className="flex-col-c-m size2 how-countdown">
+              <div className="l1-txt3 p-b-9 days">{timeRemaining.days}</div>
+              <div className="s1-txt1">Days</div>
             </div>
+
+            <div className="flex-col-c-m size2 how-countdown">
+              <div className="l1-txt3 p-b-9 hours">{timeRemaining.hours}</div>
+              <div className="s1-txt1">Hours</div>
+            </div>
+
+            <div className="flex-col-c-m size2 how-countdown">
+              <div className="l1-txt3 p-b-9 minutes">{timeRemaining.minutes}</div>
+              <div className="s1-txt1">Minutes</div>
+            </div>
+
+            <div className="flex-col-c-m size2 how-countdown">
+              <div className="l1-txt3 p-b-9 minutes">{timeRemaining.seconds}</div>
+              <div className="s1-txt1">Seconds</div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-export default UnderConstructionPage;
+export default UnderConstructionPage
--- src/app/components/UnderConstructionPage/js/script.js
+++ src/app/components/UnderConstructionPage/js/script.js
@@ -1,27 +1,26 @@
-
 // Function to calculate time remaining from a specific event
 export function calculateTimeRemaining(eventDate) {
-    const currentTime = new Date();
-    const difference = eventDate.getTime() - currentTime.getTime();
+  const currentTime = new Date()
+  const difference = eventDate.getTime() - currentTime.getTime()
 
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / (1000));
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000)
 
-    return { days, hours, minutes, seconds };
+  return { days, hours, minutes, seconds }
 }
 
 // Function to continuously update the time remaining
 export function startCountdown(eventDate, updateCallback) {
-    // Update initially
-    updateCallback(calculateTimeRemaining(eventDate));
+  // Update initially
+  updateCallback(calculateTimeRemaining(eventDate))
 
-    // Update every second
-    const intervalId = setInterval(() => {
-        updateCallback(calculateTimeRemaining(eventDate));
-    }, 1000);
+  // Update every second
+  const intervalId = setInterval(() => {
+    updateCallback(calculateTimeRemaining(eventDate))
+  }, 1000)
 
-    // Return the interval ID in case you want to stop the countdown later
-    return intervalId;
+  // Return the interval ID in case you want to stop the countdown later
+  return intervalId
 }
