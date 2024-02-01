0289de2d2605066508ae43c9383dde480d925e63
SATYAM KUMAR <satyamx10@gmail.com>
2024-02-02 02:40:22 +0530

    BI-54 : CSS file compromised

--- src/app/components/UnderConstructionPage/UnderConstructionPage.js
+++ src/app/components/UnderConstructionPage/UnderConstructionPage.js
@@ -1,6 +1,4 @@
-import './css/main.css'
-import './css/util.css'
-import './css/bootstrap.css'
+import './css/style.css'
 
 import React, { useEffect, useState, useMemo } from 'react'
 import { calculateTimeRemaining, startCountdown } from './js/script'
@@ -19,36 +17,35 @@ function UnderConstructionPage() {
   }, [eventDate])
   return (
     <div className="UnderConstructionPage">
-      <div className="bg-g1 size1 flex-w flex-col-c-sb p-l-15 p-r-15 p-t-55 p-b-35 respon1">
-        <div className="flex-col-c p-t-50 p-b-50">
-          <h3 className="l1-txt1 txt-center p-b-10">Coming Soon</h3>
 
-          <p className="txt-center l1-txt2 p-b-60">Our website is under construction</p>
+      <h3 className="headline">Coming Soon</h3>
 
-          <div className="flex-w flex-c cd100 p-b-82">
-            <div className="flex-col-c-m size2 how-countdown">
-              <div className="l1-txt3 p-b-9 days">{timeRemaining.days}</div>
-              <div className="s1-txt1">Days</div>
-            </div>
+      <p className="info">Our website is under construction</p>
 
-            <div className="flex-col-c-m size2 how-countdown">
-              <div className="l1-txt3 p-b-9 hours">{timeRemaining.hours}</div>
-              <div className="s1-txt1">Hours</div>
-            </div>
+      <div className="countdown">
 
-            <div className="flex-col-c-m size2 how-countdown">
-              <div className="l1-txt3 p-b-9 minutes">{timeRemaining.minutes}</div>
-              <div className="s1-txt1">Minutes</div>
-            </div>
+        <div className="countdown-numbers">
+          <div className="time">{timeRemaining.days}</div>
+          <div className="time-unit">Days</div>
+        </div>
 
-            <div className="flex-col-c-m size2 how-countdown">
-              <div className="l1-txt3 p-b-9 minutes">{timeRemaining.seconds}</div>
-              <div className="s1-txt1">Seconds</div>
-            </div>
-          </div>
+        <div className="countdown-numbers">
+          <div className="time">{timeRemaining.hours}</div>
+          <div className="time-unit">Hours</div>
+        </div>
+
+        <div className="countdown-numbers">
+          <div className="time">{timeRemaining.minutes}</div>
+          <div className="time-unit">Minutes</div>
+        </div>
+
+        <div className="countdown-numbers">
+          <div className="time">{timeRemaining.seconds}</div>
+          <div className="time-unit">Seconds</div>
         </div>
       </div>
     </div>
+
   )
 }
 
