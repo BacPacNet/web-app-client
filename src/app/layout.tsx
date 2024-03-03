81b5d48834b9791af1db95f53f4aed3f1004555c
Ayush Tiwari <ayushtiwari110@yahoo.com>
2024-03-03 12:10:10 +0530

    BI-41:prettier fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,26 +8,25 @@ type FontClassName = string
 const inter = Inter({ subsets: ['latin'] }) as { className: FontClassName }
 
 export const metadata: Metadata = {
-  title: "UniBuzz",
-  description: "Connect with universities and students around the world.",
-  generator: "Next.js",
-  manifest: "/manifest.json",
-  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
-  themeColor: [{ media: "(prefers-color-scheme: light)", color: "#6744ff" }],
+  title: 'UniBuzz',
+  description: 'Connect with universities and students around the world.',
+  generator: 'Next.js',
+  manifest: '/manifest.json',
+  keywords: ['nextjs', 'nextjs13', 'next13', 'pwa', 'next-pwa'],
+  themeColor: [{ media: '(prefers-color-scheme: light)', color: '#6744ff' }],
   authors: [
-    { name: "" },
+    { name: '' },
     {
-      name: "",
-      url: "",
+      name: '',
+      url: '',
     },
   ],
-  viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  viewport: 'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
   icons: [
-    { rel: "apple-touch-icon", url: "icons/icon-192x192.png" },
-    { rel: "icon", url: "icons/icon-192x192.png" },
+    { rel: 'apple-touch-icon', url: 'icons/icon-192x192.png' },
+    { rel: 'icon', url: 'icons/icon-192x192.png' },
   ],
-};
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
