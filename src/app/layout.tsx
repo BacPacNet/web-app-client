01ac4c5ba20cf412c6b5c6f0cd36810d6dcfe0ca
Ayush Tiwari <ayushtiwari110@yahoo.com>
2024-03-03 12:04:38 +0530

    BI-41:configure pwa for web-app-client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,33 @@
 import './globals.css'
 
 import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 
 type FontClassName = string
 
 const inter = Inter({ subsets: ['latin'] }) as { className: FontClassName }
 
-interface Metadata {
-  title: string
-  description: string
-}
-
 export const metadata: Metadata = {
-  title: 'BacPac',
-  description: 'Connect with Universities',
-}
+  title: "UniBuzz",
+  description: "Connect with universities and students around the world.",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: light)", color: "#6744ff" }],
+  authors: [
+    { name: "" },
+    {
+      name: "",
+      url: "",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-192x192.png" },
+    { rel: "icon", url: "icons/icon-192x192.png" },
+  ],
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
