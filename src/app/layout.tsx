537f7cd138da177d00e187f9bb493cc2462b4e57
bacpactech <bacpactech@gmail.com>
2024-04-04 22:35:21 +0530

    fix: added apollo client boiler plate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
+import { ApolloWrapper } from '@/lib/apollo-provider'
 
 type FontClassName = string
 
@@ -31,7 +32,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ApolloWrapper>{children}</ApolloWrapper>
+      </body>
     </html>
   )
 }
