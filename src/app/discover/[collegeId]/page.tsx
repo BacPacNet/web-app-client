721b40fee62d10838063bd9b8a5447d9ec80e55c
SATYAM KUMAR <satyamx10@gmail.com>
2024-04-19 03:59:48 +0530

    img responsive

--- src/app/discover/[collegeId]/page.tsx
+++ src/app/discover/[collegeId]/page.tsx
@@ -5,40 +5,10 @@ import Image from 'next/image'
 import Reviews from '@/components/Reviews/page'
 import StarsRating from '@/components/Stars/page'
 import ContactCard from '@/components/ContactCard/page'
-
+import Data from './data'
 export default function CollegeDiscovery() {
-  const college = {
-    name: 'Nagoya University',
-    img: 'https://github.com/BacPacNet/web-app-client/blob/BI-64_college_discovery_page/public/demo_cd/img.png?raw=true',
-    logo: 'https://github.com/BacPacNet/web-app-client/blob/BI-64_college_discovery_page/public/demo_cd/Logo.png?raw=true',
-    contact: {
-      link: 'https://en.nagoya-u.ac.jp/index.html',
-      officeHours: 'Monday to Friday 9:00 am - 12:00 p.m. and 1:00 p.m - 5:00 p.m',
-      email: 'apply@g30.nagoya-u.ac.jp',
-      phone: 'TEL:+81-52-747-6556',
-      fax: 'TEL:+81-52-747-6556 ',
-      location: 'Furo-cho, Chikusa-ku, Nagoya, 464-8601, Japan',
-    },
-    para1: `Nagoya University(名古屋大学,Nagoya daigaku), abbreviated toMeidai(名大)orNU, is aJapanese Research University located in Chikusa-ku,
-    Japan. Nagoya University, since its foundation in 1939, is one of seven imperial universities which has developed into one of the
-    nation’s leading universities. This success is attributed to the liberal and vibrant character of Nagoya University which encourages
-    education and research that is free from traditional and rigid thinking.`,
-    para2: ` Nagoya University is one of the top ranked universities in Japan. Here, students and scholars work together, engage in top-notch
-    research to pursue scientific discoveries and invent new state-of-the-art technologies. Six Nobel Prizes have been awarded to scholars
-    associated with Nagoya University.`,
-    para3: ` Today, Nagoya University is internationalizing the campus and promoting diversity. For more than a decade, we have offered
-    English-taught programs as an essential part of our mission. Just before the COVID-19 pandemic, the number of international students
-    reached almost 3,000. Students came from over 100 countries and regions. Although the number of students dropped because of the
-    pandemic, the number of applicants for the Global 30 international Programs hit a record high in 2021. We continue to promote
-    internationalization.`,
-  }
-  const statsImg = {
-    a: 'https://github.com/BacPacNet/web-app-client/blob/BI-64_college_discovery_page/public/demo_cd/1.png?raw=true',
-    b: 'https://github.com/BacPacNet/web-app-client/blob/BI-64_college_discovery_page/public/demo_cd/2.png?raw=true',
-    c: 'https://github.com/BacPacNet/web-app-client/blob/BI-64_college_discovery_page/public/demo_cd/4.png?raw=true',
-    d: 'https://github.com/BacPacNet/web-app-client/blob/BI-64_college_discovery_page/public/demo_cd/3.png?raw=true',
-  }
-
+  const college = Data.collegeData
+  const statsImg = Data.statsImgData
   return (
     <div className="w-screen min-h-screen bg-white text-black">
       {/* breadcrumb */}
@@ -50,8 +20,8 @@ export default function CollegeDiscovery() {
         <span className="text-lg text-purple-600">{college.name}</span>
       </div>
       {/* Image */}
-      <div className="relative" style={{ height: '500px', width: '100vw', overflow: 'hidden' }}>
-        <Image src={college.img} alt="" layout="fill" objectFit="cover" />
+      <div>
+        <Image src={college.img} alt="College logo" layout="responsive" width={0} height={0} />
       </div>
       {/* Community Info */}
       <div className="px-8 lg:px-28 grid grid-cols-1 md:grid-cols-2 justify-center md:justify-between py-10 gap-8">
