2a952f6d9dca4c6b25763ebf5e8d8abc24636ade
Ayush Tiwari <ayushtiwari110@yahoo.com>
2024-06-20 12:13:32 +0530

    refactor auth logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { isAuthenticated } from './utils/isAuthenticated'
 
 const protectedRoutes = ['/community', '/communityuniversity']
 export default function middleware(req: NextRequest) {
-  const user = req.cookies.get('uni_user_token')
-  if (
-    !user?.value &&
-    (protectedRoutes.includes(req?.nextUrl?.pathname) || protectedRoutes.some((route) => req?.nextUrl?.pathname.startsWith(route)))
-  ) {
+  if (!isAuthenticated(req) && protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
     const absoluteUrl = new URL('/login', req.nextUrl.origin)
     return NextResponse.redirect(absoluteUrl.toString())
   }
