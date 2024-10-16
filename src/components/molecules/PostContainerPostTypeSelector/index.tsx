d6468198a427f04c332cc0d7dd20d19e36fa71c3
bacpactech <bacpactech@gmail.com>
2024-10-16 19:23:23 +0530

    fix: UI fixes

--- src/components/molecules/PostContainerPostTypeSelector/index.tsx
+++ src/components/molecules/PostContainerPostTypeSelector/index.tsx
@@ -7,7 +7,7 @@ const PostContainerPostTypeSelector = () => {
   const [selectedType, setSelectedType] = useState<string>('RECENT')
 
   return (
-    <div className="flex h-16 bg-white mt-5 rounded-t-2xl border-b-2 border-neutral-200 ">
+    <div className="flex h-16 bg-white rounded-t-2xl border-b-2 border-neutral-200 ">
       {PostType.map((item) => (
         <button
           onClick={() => setSelectedType(item)}
@@ -15,7 +15,6 @@ const PostContainerPostTypeSelector = () => {
           className={`relative w-28 px-4 py-2 ${selectedType === item ? 'text-[#3A169C]' : 'text-gray-500 text-neutral-400'} font-extrabold `}
         >
           {item}
-          {selectedType === item && <span className="absolute left-1/4 bottom-3 w-14 h-[2px] bg-[#6744FF]"></span>}
         </button>
       ))}
     </div>
