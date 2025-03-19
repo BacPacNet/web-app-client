bd7bc8b9aa5c640e5cfbed4db52894e1a81dad5c
bacpactech <bacpactech@gmail.com>
2025-03-19 17:00:19 +0530

    fix: add user guideline

--- src/app/user-guidelines/page.tsx
+++ src/app/user-guidelines/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Userguidelines() {
   return (
     <main className="max-width-allowed mx-auto md:px-0 px-4 pt-8 pb-4 text-justify">
-      <h1 className="font-poppins md:text-4xl text-md-big font-bold text-neutral-700 mb-6">User Guidelines</h1>
+      <h1 className="font-poppins md:text-4xl text-md-big  font-bold text-neutral-700 mb-6">User Guidelines</h1>
 
       <p className="text-neutral-500 font-normal md:text-xs text-2xs mb-4">Effective Date: August 1, 2024</p>
       <p className="text-neutral-600 font-normal md:text-xs text-2xs mb-16">
@@ -12,7 +12,7 @@ function Userguidelines() {
 
       {/* Respectful Content */}
       <section className="mb-8">
-        <h2 className="font-poppins md:text-4xl text-md-big font-bold text-neutral-700 mb-6">Respectful Content</h2>
+        <h2 className="font-poppins text-[20px] font-bold text-neutral-700 mb-6">Respectful Content</h2>
         <ul className="space-y-3 list-disc pl-4 text-neutral-600 font-normal md:text-xs text-2xs mb-16">
           <li className="text-gray-700">
             Maintain Civility: Engage in discussions with respect and courtesy. Avoid hate speech, harassment, or any form of discrimination based on
@@ -27,7 +27,7 @@ function Userguidelines() {
 
       {/* Authentic Identity */}
       <section className="mb-8">
-        <h2 className="font-poppins md:text-4xl text-md-big font-bold text-neutral-700 mb-6">Authentic Identity</h2>
+        <h2 className="font-poppins text-[20px]  font-bold text-neutral-700 mb-6">Authentic Identity</h2>
         <ul className="space-y-3 list-disc pl-4 text-neutral-600 font-normal md:text-xs text-2xs mb-16">
           <li className="text-gray-700">
             Use Real Identities: Register with your actual name and university affiliation to build trust within the community.
@@ -41,7 +41,7 @@ function Userguidelines() {
 
       {/* Privacy and Security */}
       <section className="mb-8">
-        <h2 className="font-poppins md:text-4xl text-md-big font-bold text-neutral-700 mb-6">Privacy and Security</h2>
+        <h2 className="font-poppins text-[20px]  font-bold text-neutral-700 mb-6">Privacy and Security</h2>
         <ul className="space-y-3 list-disc pl-4 text-neutral-600 font-normal md:text-xs text-2xs mb-16">
           <li className="text-gray-700">
             Protect Personal Information: Refrain from sharing sensitive data publicly. Be mindful of your own and others&apos; privacy.
@@ -57,7 +57,7 @@ function Userguidelines() {
 
       {/* Intellectual Honesty */}
       <section className="mb-8">
-        <h2 className="font-poppins md:text-4xl text-md-big font-bold text-neutral-700 mb-6">Intellectual Honesty</h2>
+        <h2 className="font-poppins text-[20px]  font-bold text-neutral-700 mb-6">Intellectual Honesty</h2>
         <ul className="space-y-3 list-disc pl-4 text-neutral-600 font-normal md:text-xs text-2xs mb-16">
           <li className="text-gray-700">Avoid Misinformation: Share accurate information and refrain from spreading false or misleading content.</li>
           <li className="text-gray-700">
@@ -69,7 +69,7 @@ function Userguidelines() {
 
       {/* Appropriate Content Sharing */}
       <section className="mb-8">
-        <h2 className="font-poppins md:text-4xl text-md-big font-bold text-neutral-700 mb-6">Appropriate Content Sharing</h2>
+        <h2 className="font-poppins text-[20px]  font-bold text-neutral-700 mb-6">Appropriate Content Sharing</h2>
         <ul className="space-y-3 list-disc pl-4 text-neutral-600 font-normal md:text-xs text-2xs mb-16">
           <li className="text-gray-700">Relevant Contributions: Post content pertinent to student life, academics, and university events.</li>
           <li className="text-gray-700">
@@ -83,7 +83,7 @@ function Userguidelines() {
 
       {/* Collaborative Engagement */}
       <section className="mb-8">
-        <h2 className="font-poppins md:text-4xl text-md-big font-bold text-neutral-700 mb-6">Collaborative Engagement</h2>
+        <h2 className="font-poppins text-[20px]  font-bold text-neutral-700 mb-6">Collaborative Engagement</h2>
         <ul className="space-y-3 list-disc pl-4 text-neutral-600 font-normal md:text-xs text-2xs mb-16">
           <li className="text-gray-700">
             Participate Actively: Engage in group discussions, study sessions, and collaborative projects to enrich the community experience.
@@ -97,7 +97,7 @@ function Userguidelines() {
 
       {/* Compliance & Enforcement */}
       <section className="mb-8">
-        <h2 className="font-poppins md:text-4xl text-md-big font-bold text-neutral-700 mb-6">Compliance & Enforcement</h2>
+        <h2 className="font-poppins text-[20px]  font-bold text-neutral-700 mb-6">Compliance & Enforcement</h2>
         <ul className="space-y-3 list-disc pl-4 text-neutral-600 font-normal md:text-xs text-2xs mb-16">
           <li className="text-gray-700">Follow Platform Rules: Adherence to these guidelines is mandatory for all users.</li>
           <li className="text-gray-700">
