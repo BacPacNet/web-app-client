dcc9fe68f92629b25635c1ac3a41f689c9def30a
Pratik Yadav <masterpratikyadav@gmail.com>
2025-09-09 21:52:48 +0530

    fix: community UI

--- src/components/atoms/GroupAvatarWithBadge/index.tsx
+++ src/components/atoms/GroupAvatarWithBadge/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import { IoIosPeople } from 'react-icons/io'
+import officialLogo from '@assets/official-logo.svg'
 
 interface Props {
   groupLogoUrl: string
@@ -12,7 +13,7 @@ const GroupAvatarWithBadge: React.FC<Props> = ({ groupLogoUrl, isOfficial, badge
   return (
     <div
       className={`relative z-1 flex items-center justify-center  rounded-full w-11 h-11 lg:w-14 lg:h-14 overflow-visible ${
-        isOfficial ? 'border-2 border-primary-500' : ''
+        isOfficial ? 'border-2 border-neon' : ''
       }`}
     >
       {groupLogoUrl ? (
@@ -28,8 +29,8 @@ const GroupAvatarWithBadge: React.FC<Props> = ({ groupLogoUrl, isOfficial, badge
       )}
 
       {isOfficial && badgeLogoUrl && (
-        <div className="absolute bg-white -bottom-2 w-5 h-5 border-2 border-primary-500 rounded-full flex justify-center items-center">
-          <Image src={badgeLogoUrl} alt="Badge" width={12} height={12} className="rounded-full object-contain" />
+        <div className="absolute bg-white -bottom-2 w-5 h-5 border-2 border-neon rounded-full flex justify-center">
+          <Image className="object-contain rounded-full" src={officialLogo as string} width={12} height={12} alt="" />
         </div>
       )}
     </div>
