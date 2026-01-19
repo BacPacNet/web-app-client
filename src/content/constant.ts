dd86157f1be568768fc62dc94743b47df1e58451
Aamil13 <aamil.shafi13@gmail.com>
2026-01-19 18:38:36 +0530

    mobile-app download page in register final screen

--- src/content/constant.ts
+++ src/content/constant.ts
@@ -67,18 +67,18 @@ export enum TRACK_EVENT {
   NEW_GROUP_CHAT = 'new_group_chat',
 
   //   university community events
-  UNIVERSITY_COMMUNITY_PAGE_VIEW = 'university_community_page_view',
-  UNIVERSITY_COMMUNITY_PAGE_VIEW_DURATION = 'university_community_page_view_duration',
+  //   UNIVERSITY_COMMUNITY_PAGE_VIEW = 'university_community_page_view',
+  UNIVERSITY_COMMUNITY_PAGE_VIEW_DURATION = 'university_community_page_view',
 
   //   groups events
   SIDEBAR_GROUP_FILTER = 'sidebar_group_filter',
-  COMMUNITY_GROUP_PAGE_VIEW = 'community_group_page_view',
-  COMMUNITY_GROUP_PAGE_VIEW_DURATION = 'community_group_page_view_duration',
+  //   COMMUNITY_GROUP_PAGE_VIEW = 'community_group_page_view',
+  COMMUNITY_GROUP_PAGE_VIEW_DURATION = 'community_group_page_view',
   NEW_COMMUNITY_GROUP = 'new_community_group',
 
   //   register page events
-  REGISTER_PAGE_VIEW_DURATION = 'register_page_view_duration',
-  UNIVERSITY_VERIFICATION_STEP_VIEW_DURATION = 'university_verification_step_view_duration',
+  REGISTER_PAGE_VIEW_DURATION = 'register_page_view',
+  UNIVERSITY_VERIFICATION_STEP_VIEW_DURATION = 'university_verification_step_view',
 
   //   error page events
   ERROR_PAGE_VIEW = 'error_page_view',
